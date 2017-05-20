26d86f47847eb2653fbe57a6fcadf708fbf609d2
Jared Silver <jared@jaredsilver.name>
2017-05-20 17:51:43 -0500

    Allow for creation of new games, implement functionality of teacher dashboard

--- src/AppBundle/Controller/TeacherGamesController.php
+++ src/AppBundle/Controller/TeacherGamesController.php
@@ -2,24 +2,25 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Game;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class TeacherGamesController extends Controller
 {
     /**
-     * @Route("/teacher/game/{gameID}")
+     * @Route("/teacher/game/{joinCode}", name="teacher_game_admin")
      */
      public function teacherGameAction()
      {
        /* Check if user is authenticated as a teacher
         *   If not, redirect to '/teacher/login'
-        * Check if gameID exists in url
+        * Check if joinCode exists in url
         *   If not, redirect to '/teacher/games' (w/o ID)
-        * Check if the gameID exists in database
+        * Check if the joinCode exists in database
         *   If not, redirect to '/teacher/games' (w/o ID)
-        * Check if gameID belongs to that teacher
+        * Check if joinCode belongs to that teacher
         *   If not, redirect to '/teacher/games' (w/o ID)
         * Display 'teacher/gameAdmin' view
         */
@@ -36,26 +37,49 @@ class TeacherGamesController extends Controller
               return $this->redirectToRoute('teacher_login');
           }
 
-         $games = $this->getDoctrine()
+          $games = $this->getDoctrine()
             ->getRepository('AppBundle:Game')
             ->findByTeacherID($this->getUser()->getID());
 
-        return $this->render(
-            'teacher/games.html.twig',
-            array('games' => $games)
-        );
+          return $this->render(
+              'teacher/games.html.twig',
+              array('games' => $games)
+          );
       }
 
       /**
        * @Route("/teacher/games/new", name="new_game")
        */
-       public function teacherNewGameAction()
+       public function teacherNewGameAction(Request $request)
        {
           if(!$this->getUser())
           {
               // TODO: set flash message here too
               return $this->redirectToRoute('teacher_login');
           }
+
+          // Create new game form
+          $game = new Game();
+          $game->setTeacherID($this->getUser()->getID());
+          $game->setJoinCode(substr(str_shuffle(str_repeat('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', 4)), 1, 4));
+          $form = $this->createFormBuilder($game)->getForm();
+
+          // Handle submit request
+          $form->handleRequest($request);
+          if ($form->isSubmitted() && $form->isValid()) {
+              // Persist data to database
+              // TODO: handle collisions on joinCode
+                // --> doesn't matter yet (statistically speaking), but this is really poor code if left like this before launch
+              $em = $this->getDoctrine()->getManager();
+              $em->persist($game);
+              $em->flush();
+              return $this->redirectToRoute('teacher_game_admin', array('joinCode' => $game->getJoinCode()));
+          }
+
+          return $this->render(
+              'teacher/new_game.html.twig',
+              array('form' => $form->createView())
+          );
           return $this->render('teacher/new_game.html.twig');
        }
 }
